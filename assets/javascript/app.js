268ba2643ecb7452e704a14bfd90c953740f646f
jaro0024 <30360520+jaro0024@users.noreply.github.com>
2018-01-05 13:24:28 -0500

    changed some formatting on html and worked on the styling of page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,60 @@
 
-var topics = ["golf", "MLB", "NBA", "NCAA basketball", "NCAA football", "NFL","NHL", "soccer", "tennis", "volleyball"];
+var topics = ["Golf", "MLB", "NBA", "NCAA basketball", "NCAA football", "NFL", "NHL", "Soccer", "Tennis", "Volleyball"];
 
-function displaySportsImages () {
+function displaySportsImages() {
 
-    $("button").on("click", function() {
+    $("button").on("click", function () {
 
-    var sport = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var sport = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).done(function(response) {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
 
-        var results = response.data;
+            var results = response.data;
 
-        for (var i = 0; i < results.length; i++) {
+            for (var i = 0; i < results.length; i++) {
 
-            var sportsDiv = $("<div class='sport'>");
+                var sportsDiv = $("<div class='sport'>");
 
-            var p = $("<p>").text("Rating: " + results[i].rating);
+                var p = $("<p>").text("Rating: " + results[i].rating);
 
-            var sportsImage = $("<img>");
+                var sportsImage = $("<img>");
 
-            sportsImage.addClass("sportImg");
-        
-            sportsImage.attr("src",results[i].images.fixed_height_still.url);
+                sportsImage.addClass("sportImg");
 
-            sportsImage.attr("data-still", results[i].images.fixed_height_still.url);
+                sportsImage.attr("src", results[i].images.fixed_height_still.url);
 
-            sportsImage.attr("data-animate", results[i].images.fixed_height.url);
+                sportsImage.attr("data-still", results[i].images.fixed_height_still.url);
 
-            sportsImage.attr("data-state", "still");
+                sportsImage.attr("data-animate", results[i].images.fixed_height.url);
 
-            sportsDiv.prepend(p);
-            sportsDiv.prepend(sportsImage);
+                sportsImage.attr("data-state", "still");
 
-            $("#sports-view").prepend(sportsDiv);
-          }
+                sportsDiv.prepend(p);
+                sportsDiv.prepend(sportsImage);
 
-      });
+                $("#sports-view").prepend(sportsDiv);
+            }
+
+        });
     });
 }
 
-$(document).on("click", ".sportImg", function() {
+$(document).on("click", ".sportImg", function () {
 
     var state = $(this).attr("data-state");
-    
+
     if (state == "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
     } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
     }
-  });
+});
 
 function renderButtons() {
 
@@ -62,24 +62,24 @@ function renderButtons() {
 
     for (var i = 0; i < topics.length; i++) {
 
-      var a = $("<button>");
-      a.addClass("sport");
-      a.attr("data-name", topics[i]);
-      a.text(topics[i]);
-      $("#buttons-view").append(a);
+        var a = $("<button>");
+        a.addClass("sport");
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
+        $("#buttons-view").append(a);
     }
-  }
+}
 
-  $("#add-sport").on("click", function(event) {
+$("#add-sport").on("click", function (event) {
     event.preventDefault();
     var sport = $("#sport-input").val().trim();
     topics.push(sport);
 
     renderButtons();
-  });
+});
 
-  $(document).on("click", ".sport", displaySportsImages);
+$(document).on("click", ".sport", displaySportsImages);
 
-  renderButtons();
+renderButtons();
 
 displaySportsImages();
